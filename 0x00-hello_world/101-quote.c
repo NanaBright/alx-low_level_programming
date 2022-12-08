910cf9c12a6cc887d1e07c85adcb516bd6b3c4a3
Bright Yanchirah <nanabryte.nb@gmail.com>
2022-12-08 08:06:55 -0800

    .

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Starting point
+ *
+ * Return: Always 0 if compile succeeds
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n",59):
+		retun(0);
+}
