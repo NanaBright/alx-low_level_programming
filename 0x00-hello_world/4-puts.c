7194003ad381ed423f3deb6d00f511194b267837
Bright Yanchirah <nanabryte.nb@gmail.com>
2022-12-08 04:15:44 -0800

    tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-*main - used to print the phrase
-*Return - zero will be returned if exited with no errors
-*/
+* main - Entry point
+* Return: Always 0 (Success)
+ * */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
