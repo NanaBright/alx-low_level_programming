c2226250cd0273f063a4e1db4616ce04f4383b28
Bright Yanchirah <nanabryte.nb@gmail.com>
2022-12-08 04:08:26 -0800

    tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - used to print the phrase
+*Return - zero will be returned if exited with no errors
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
