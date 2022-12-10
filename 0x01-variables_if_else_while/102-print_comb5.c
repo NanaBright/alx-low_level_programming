39c35db8e276702da79ab2da1f0f0299dfd20c77
Bright Yanchirah <nanabryte.nb@gmail.com>
2022-12-10 06:52:55 -0800

    .

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,18 +8,21 @@ int main(void)
 {
 	int x, y;
 
-	for (x = 0; x <= 0; x++)
+	for (x = 0; x <= 99; x++)
 	{
 		for (y = x + 1; y <= 99; y++)
-		putchar(x / 10 + '0');
-		putchar(x % 10 + '0');
-		putchar(' ');
-		putchar(y / 10 + '0');
-		putchar(y % 10 + '0');
-		if (!(x == 98 && y == 99))
 		{
-			putchar(',');
+			putchar(x / 10 + '0');
+			putchar(x % 10 + '0');
 			putchar(' ');
+			putchar(y / 10 + '0');
+			putchar(y % 10 + '0');
+		
+			if (!(x == 98 && y == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
