2e65280e4274586d66fee1ebd69ba7d7857a072c
Bright Yanchirah <nanabryte.nb@gmail.com>
2022-12-10 05:54:45 -0800

    .

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,13 +7,14 @@
 int main(void)
 {
 	int x, y, z;
+
 	for (x = 48; x <= 57;  x++)
 	{
 		for (y = 48; y <= 57; y++)
 		{
 			for (z = 48; z <= 57; z++)
 			{
-				if (x == y || x ==z || y == z || y <=x || z <= y)
+				if (x == y || x == z || y == z || y <= x || z <= y)
 				{
 				}
 				else
@@ -21,7 +22,7 @@ int main(void)
 					putchar(x);
 					putchar(y);
 					putchar(z);
-					if (x == 55 && y== 56 && z == 57)
+					if (x == 55 && y == 56 && z == 57)
 					{
 					}
 					else
