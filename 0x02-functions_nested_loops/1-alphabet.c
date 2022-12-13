d175ce9224e6fef81c241a7f47faef66e59f1186
Bright Yanchirah <nanabryte.nb@gmail.com>
2022-12-13 14:15:07 -0800

    .

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * prints alphabets
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		_putchar(x);
+		x++;
+	}
+	_putchar('\n');
+}
