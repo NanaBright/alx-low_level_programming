567ae550e126e7d2a79b281fac22e5a564111dc5
Bright Yanchirah <nanabryte.nb@gmail.com>
2022-12-13 19:36:36 -0800

    .

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 	int x = 0, y = 1, next = 0;
-  	int sum = 0;
+	int sum = 0;
 
 	while (next < 4000000)
 	{
-		next = x +y;
+		next = x + y;
 		x = y;
 		y = next;
 		if (next % 2 == 0)
