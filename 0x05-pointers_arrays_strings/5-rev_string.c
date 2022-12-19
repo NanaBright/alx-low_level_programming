3a9bf7970fb8c4ad0e911a1c3e439ede79d62fc8
Bright Yanchirah <nanabryte.nb@gmail.com>
2022-12-19 09:19:27 -0800

    .

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,20 +9,23 @@
 
 void rev_string(char *s)
 {
-	int i, x, y;
+	int i, len, len1;
 	char temp;
 
-	for (x = 0; s[x] != '\0'; x++)
-		;
+	len = 0;
+	len1 = 0;
 
-	i = 0;
-	y = x / 2;
-
-	while (y--)
+	while (s[len] != '\0')
 	{
-		temp = s[x];
-		s[i] = s[x - i - 1];
-		s[x - i - 1] = temp;
-		i++;
+		len++;
+	}
+
+	len1 len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len1];
+		s[len1--] = temp;
 	}
 }
