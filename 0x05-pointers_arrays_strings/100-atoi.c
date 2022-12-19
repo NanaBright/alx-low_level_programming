8bec674350681010ea105797980f1eae35c4591a
Bright Yanchirah <nanabryte.nb@gmail.com>
2022-12-19 12:43:08 -0800

    .

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -21,7 +21,7 @@ int _atoi(char *s)
 	while (s[len] != '\0')
 		len++;
 
-	while(i < len && f == 0)
+	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
@@ -31,7 +31,7 @@ int _atoi(char *s)
 			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
-			n =n * 10 + digit;
+			n = n * 10 + digit;
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
