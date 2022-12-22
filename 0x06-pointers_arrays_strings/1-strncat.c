60831f8206b8ea3dbdbf3c57cf42187d8aee2c23
Bright Yanchirah <nanabryte.nb@gmail.com>
2022-12-22 13:56:41 -0800

    .

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copies a string
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes to concat
+ * Return: result to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
