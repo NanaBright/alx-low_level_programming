3ae3212ea56209ad0bcc01e08d6970c63299dd67
Bright Yanchirah <nanabryte.nb@gmail.com>
2022-12-22 14:07:35 -0800

    .

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,9 +12,8 @@ char *_strcat(char *dest, char *src)
 	int i = 0, j = 0;
 
 	while (dest[i] != '\0')
-	{
 		i++;
-	}
+	
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
@@ -23,5 +22,6 @@ char *_strcat(char *dest, char *src)
 	}
 	
 	dest[i] = '\0';
+
 	return (dest);
 }
