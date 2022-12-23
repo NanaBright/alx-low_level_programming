fc9e5c431fc3ed1d51b78f5f09f5a839b64f1b8f
Bright Yanchirah <nanabryte.nb@gmail.com>
2022-12-22 18:55:12 -0800

    .

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,8 +9,8 @@
 char *cap_string(char *s)
 {
 	int count = 0, i;
+	char spe[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	char spe[13] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 	if (*(s + count) >= 97 && *(s + count) <= 122)
 		*(s + count) = *(s + count) - 32;
 	count++;
@@ -18,10 +18,10 @@ char *cap_string(char *s)
 	{
 		for (i = 0; i < 13; i++)
 		{
-			if (*(s + count) == spe_words[i])
+			if (*(s + count) == spe[i])
 			{
 				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count +1)) - 32;
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
 				break;
 			}
 		}
