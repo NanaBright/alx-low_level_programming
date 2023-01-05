457f5dd41565c923af9989e0f4736969fdbd7ff6
Bright <nanabryte.nb@gmail.com>
2023-01-05 04:25:30 -0800

    main.h

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char);
+void _puts_recursion(char *s);
+void _print_rev_recurssion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_reursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(int *s);
+int wildcmp(char *s1, char *s2:;
+
+#endif
