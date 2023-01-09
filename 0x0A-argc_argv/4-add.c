f2762d096c6449ae42d5814ede17dfbb789e4157
Bright <nanabryte.nb@gmail.com>
2023-01-09 07:44:29 -0800

    0 - 100

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,84 +1,32 @@
 #include <stdio.h>
-#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
 
-/**
- * _atoi - converts string into integer
- * @s: string
- * Return: converted int
- */
-
-int _atoi(char *s)
-{
-	int i, d, n len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
-}
 /**
  * main - adds positive numbers
  * @argc: command line arguments
  * @argv: contains program commands
- * Return: 0
+ * Return: 0 or 1
  */
 
 int main(int argc, char *argv[])
 {
-	int sum, num, i, j, k;
+	int sum, i, j;
 
 	sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (j = 0; argv[x][j] != '\0'; j++)
 		{
-			if (argv[i][j] > '9' || argv[i][j] < '0')
+			if (!isdigit(argv[i][j]))
 			{
-				puts("Error");
+				printf("Error\n");
 				return (1);
 			}
 		}
+		sum += atoi(argv[i]);
 	}
-
-	for (k = 1; k < argc; k++)
-	{
-		num = _atoi(argv[k]);
-		if (num >= 0)
-		{
-			sum += num;
-		}
-	}
-
 	printf("%d\n", sum);
 	return (0);
 }
