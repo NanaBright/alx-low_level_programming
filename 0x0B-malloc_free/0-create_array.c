7a6c8fa8aa43376541693acd4fa191f3ab302516
Bright <nanabryte.nb@gmail.com>
2023-01-10 16:17:21 -0800

    .

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdllib.h>
+
+/**
+ * create_array - creates an array
+ * @size: size of aray
+ * @c: starting char
+ * Return: pointer of the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	*p = malloc(sizeof(c) * size);
+
+	if (*p == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		*p[i] = c;
+
+	return (*p);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - opies string given as a parameter
+ * @str: string
+ * Return: poitner of an array
+ */
+
+char *strdup(char *str)
+{
+	char *dup;
+	unsigned int i, j;
+
+	i = 0;
+	j = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[j])
+		j++;
+	
+	dup = malloc(sizeof(char) * (j + 1));
+
+	if (dup == NULL)
+		return (NULL);
+
+	while ((dup[i] = str[i]) != '\0')
+		i++;
+
+	return (dup);
+}
