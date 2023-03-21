ff5574e8c12c0055f0f25e852ba1cff271bd17f0
Bright Yanchirah <nanabryte.nb@gmail.com>
2023-03-20 17:36:04 -0700

    extra-files

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a strings
+ *
+ * @str: pointer
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
